b95126875841d81d589167688506dc3c5f14420c
ivank <ivankaitazoff@gmail.com>
2022-03-17 22:51:13 -0300

    script file created

---
+++ script.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+menu="Menu principal
+Opcion 1) Configurar variables
+Opcion 2) Obtener Informe de la letra
+Opcion 3) Guardar Informe
+Opcion 4) Cambiar propietarios y permiso
+opcion 5) Salir"
+
+opcion=0
+
+echo "$menu"
+while 
+	$opcion==0
+do
+	read -p
+
+function configurarVariables{
+	echo "configurar Variables"
+}
+
+function obtenerInformeLetra{
+	echo 'obtenerInformeLetra'
+}
+
+function obtenerInforme{
+	echo obtenerInforme
+}
+
+function guardarInforme{
+	echo guardarInforme
+}
+
+function cambiarPropietariosYPermiso{
+	echo cambiarPropietarios
+}
+
+function salir{
+	echo salir
+}
