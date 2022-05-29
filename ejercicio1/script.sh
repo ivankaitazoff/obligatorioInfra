d6d886152e0750b210d02baa75a5908ab0aa8559
kali <ivankaitazoff@gmail.com>
2022-05-29 00:00:42 -0300

    Anda bien el menu del ejercicio 1

--- ejercicio1/script.sh
+++ ejercicio1/script.sh
@@ -1,39 +1,19 @@
 #!/bin/bash
-menu="Menu principal
-Opcion 1) Configurar variables
-Opcion 2) Obtener Informe de la letra
-Opcion 3) Guardar Informe
-Opcion 4) Cambiar propietarios y permiso
-opcion 5) Salir
-Ingresar una opcion"
-
-
-echo "$menu"
-
-read opcion
-echo "La opcion elegida es: " $opcion
-
-while 
-	$opcion==0
-do
-	read opcion
 
 function configurarVariables(){
 	echo "configurar variables"
 }
 
-configurarVariables
-
 function obtenerInformeLetra(){
-	echo 'obtener Informe Letra'
+	echo "obtener Informe Letra"
 }
 
 function obtenerInforme(){
-	echo obtener Informe
+	echo "obtener Informe"
 }
 
 function guardarInforme(){
-	echo guardar Informe
+	echo "guardar Informe"
 }
 
 function cambiarPropietariosYPermiso(){
@@ -41,15 +21,39 @@ function cambiarPropietariosYPermiso(){
 }
 
 function salir(){
-	echo salir
-	exit 1;
+	echo "salir"
 }
-if [$opcion == "1"]; 
-then
-	echo "Opcion 1 elegida"
-	configurarVariables
-else
-	obtenerInformeLetra
-fi
 
-salir
+menu=("Configurar variables"
+"Obtener Informe de la letra"
+"Guardar Informe"
+"Cambiar propietarios y permiso"
+"Salir")
+
+
+echo "$menu"
+PS3='Ingrese su eleccion: '
+select opt in "${menu[@]}"
+do
+	case $opt in
+		"Configurar variables")
+			echo "usted eligio 1"
+			configurarVariables
+			;;
+		"Obtener Informe de la letra")
+			echo "usted eligio 2"
+			;;
+		"Guardar Informe")
+			echo "Usted eligio 3"
+			;;
+		"Cambiar propietarios y permiso")
+			echo "usted eligio 4"
+			;;
+		"Salir")
+			break
+			;;
+		*) echo "Opcion invalida $REPLY";;
+	esac
+done
+
+
